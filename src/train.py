af876aa12f733740946ff455b556b4a77b14b280
domenicocinque <domenico.cinque98@gmail.com>
2023-03-26 16:57:08 +0200

    working version

--- src/train.py
+++ src/train.py
@@ -14,8 +14,6 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision.datasets import MNIST
 
-from src.utils import plot_latent_space
-
 
 class Encoder(nn.Module):
     def __init__(self, hidden_size=32):
@@ -55,15 +53,19 @@ class Decoder(nn.Module):
 
 
 class ConvAutoencoder(pl.LightningModule):
-    def __init__(self, hidden_size=32, anchors=None):
+    def __init__(
+        self, hidden_size: int = 32, anchors: torch.Tensor = None, use_relative_space: bool = False
+    ):
         super().__init__()
         self.encoder = Encoder(hidden_size=hidden_size)
         self.decoder = Decoder(hidden_size=hidden_size)
         self.anchors = anchors
+        self.use_relative_space = use_relative_space
 
     def forward(self, x):
         x = self.encoder(x)
-        x = self.relative_projection(x, self.anchors)
+        if self.use_relative_space:
+            x = self.relative_projection(x, self.anchors)
         x = self.decoder(x)
         return x
 
@@ -91,12 +93,6 @@ class ConvAutoencoder(pl.LightningModule):
         return optim.Adam(self.parameters(), lr=1e-3)
 
 
-# We need to select the anchors for the relative projection
-# The anchors are points in the training dataset that we want to use as reference points
-# We first define a function that selects n_anchors at random from the training dataset
-# and saves them in a way that we can load them later.
-
-
 def select_random_anchors(
     train_dataset: Dataset,
     n_anchors: int = 10,
@@ -108,15 +104,14 @@ def select_random_anchors(
     for i, (img, label) in enumerate(train_loader):
         if i == n_anchors:
             break
-        torchvision.utils.save_image(img, f"{data_dir}/{anchors_dir}/{label.item()}.png")
+        torchvision.utils.save_image(img, f"{data_dir}/{anchors_dir}/anchor_{i}.png")
 
 
 def load_anchors(data_dir: str = "data", anchors_dir: str = "anchors"):
     """Load the anchors from the anchors_dir folder."""
     anchors = []
-    for i in range(10):
-        img = torchvision.io.read_image(f"{data_dir}/{anchors_dir}/{i}.png")
-        img = transforms.ToTensor()(img)
+    for anchor in os.listdir(f"{data_dir}/{anchors_dir}"):
+        img = torchvision.io.read_image(f"{data_dir}/{anchors_dir}/{anchor}")
         anchors.append(img)
     return torch.stack(anchors)
 
@@ -130,18 +125,67 @@ def load_anchors(data_dir: str = "data", anchors_dir: str = "anchors"):
 
 
 def train(seed=42, data_dir="data", anchors_dir="anchors"):
+    """Train the ConvAutoencoder model and saves weights in the weights folder."""
     pl.seed_everything(seed)
+    N_ANCHORS = 10
     train_dataset = MNIST(data_dir, train=True, download=True, transform=transforms.ToTensor())
     val_dataset = MNIST(data_dir, train=False, download=True, transform=transforms.ToTensor())
 
     if not os.path.exists(f"{data_dir}/{anchors_dir}"):
         os.makedirs(f"{data_dir}/{anchors_dir}")
         select_random_anchors(
-            train_dataset, n_anchors=10, data_dir=data_dir, anchors_dir=anchors_dir
+            train_dataset, n_anchors=N_ANCHORS, data_dir=data_dir, anchors_dir=anchors_dir
         )
 
     anchors = load_anchors(data_dir=data_dir, anchors_dir=anchors_dir)
 
-    model = ConvAutoencoder(anchors=anchors)
-    trainer = pl.Trainer(gpus=1, max_epochs=10, progress_bar_refresh_rate=20)
-    trainer.fit(model, train_dataset, val_dataset)
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+    model = ConvAutoencoder(hidden_size=N_ANCHORS, anchors=anchors)
+    trainer = pl.Trainer(max_epochs=1)
+    trainer.fit(model, train_loader, val_loader)
+
+    if not os.path.exists("weights"):
+        os.makedirs("weights")
+
+    torch.save(model.state_dict(), f"weights/conv_autoencoder_seed={seed}.pt")
+
+
+def load_weights(model, weights_path):
+    """Load the weights from the weights_path file into the model."""
+    model.load_state_dict(torch.load(weights_path))
+    return model
+
+
+def compare_stitched_models(
+    encoder_weights_path: str, decoder_weights_path: str, n_images: int = 10
+):
+    """Compose an autoencoder model from two differently trained encoder and decoder and plot
+    original image vs reconstruction for n_images samples in the validation dataset."""
+
+    model = ConvAutoencoder(hidden_size=10)
+    model.load_state_dict(torch.load(encoder_weights_path))
+    model.load_state_dict(torch.load(decoder_weights_path))
+
+    val_dataset = MNIST("data", train=False, download=True, transform=transforms.ToTensor())
+    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=True)
+
+    fig, axs = plt.subplots(2, n_images, figsize=(20, 4))
+    for i, (img, label) in enumerate(val_loader):
+        if i == n_images:
+            break
+        img_hat = model(img)
+        axs[0, i].imshow(img[0, 0], cmap="gray")
+        axs[1, i].imshow(img_hat[0, 0].detach(), cmap="gray")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    # train(seed=42, data_dir="data", anchors_dir="anchors")
+    # train(seed=43, data_dir="data", anchors_dir="anchors")
+
+    compare_stitched_models(
+        encoder_weights_path="weights/conv_autoencoder_seed=42.pt",
+        decoder_weights_path="weights/conv_autoencoder_seed=43.pt",
+    )
